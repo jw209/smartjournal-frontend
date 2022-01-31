83d888d2ae1bd069ea16d2a38b3c6209a8c3c2b6
chand1012 <chandler@chand1012.dev>
2022-01-31 15:05:13 -0500

    Add dependencies and login screen

--- App.tsx
+++ App.tsx
@@ -7,12 +7,18 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "./pages/Home";
 import Settings from "./pages/Settings";
 import Journal from "./pages/Journal";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
 
 // Define the config
 const config = {
   useSystemColorMode: false,
   initialColorMode: "dark",
 };
+
+// this will change
+const loggedIn = false;
 
 // extend the theme
 export const theme = extendTheme({ config });
@@ -22,11 +28,20 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer theme={DarkTheme}>
-      <NativeBaseProvider>
+      <NativeBaseProvider theme={theme}>
         <Drawer.Navigator initialRouteName="Home">
           <Drawer.Screen name="Home" component={Home} />
           <Drawer.Screen name="Journal" component={Journal} />
           <Drawer.Screen name="Settings" component={Settings} />
+
+          {loggedIn ? (
+            <Drawer.Screen name="Logout" component={Logout} />
+          ) : (
+            <>
+              <Drawer.Screen name="Login" component={Login} />
+              <Drawer.Screen name="Register" component={Register} />
+            </>
+          )}
         </Drawer.Navigator>
       </NativeBaseProvider>
     </NavigationContainer>
