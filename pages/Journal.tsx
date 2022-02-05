e09371e9fa3e2b23a1d82a63cf34f4108b4f6228
jw209 <jw209@uakron.edu>
2022-02-05 14:47:50 -0500

    Add text area for journal entries

--- pages/Journal.tsx
+++ pages/Journal.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { View } from "react-native";
-import { TextArea } from "native-base"
+import React, { useState } from "react";
+import { TextArea, Box, Text } from "native-base"
+import { Keyboard } from "react-native";
 
-const Journal = ({ navigation }: any) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <TextArea />
-    </View>
-  );
-};
+const Form = () => {
+  const [formData, setData] = useState({});
+
+  return <Box width="90%" mx="9" maxW="300px" paddingTop={4}>
+        <TextArea placeholder="Whats on your mind?" height="50%" onChangeText={value => setData({ ...formData,
+        journal: value
+      })} maxLength={150} color="white" onSubmitEditing={() => {
+        console.log(formData);
+      }}/>
+      <Text paddingTop={3}>Simply press 'return' to submit</Text> 
+    </Box>;
+}
+
+const Journal = () => {
+  return <Box flex={1} onTouchStart={() => Keyboard.dismiss()}>
+      <Form />
+    </Box>;
+}
 
 export default Journal;
