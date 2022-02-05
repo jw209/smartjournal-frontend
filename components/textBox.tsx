a7fd76a3b7f2a9a3f7bf36333b8bbffd50892a94
jw209 <jw209@uakron.edu>
2022-02-05 12:40:38 -0500

    Add text box component

---
+++ components/textBox.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box, TextArea } from "native-base"
+
+const Example = () => {
+    return <Box alignItems="center" w="100%">
+        <TextArea h={20} placeholder="Text Area Placeholder" w="75%" maxW="300" />
+      </Box>;
+  };
--- pages/Journal.tsx
+++ pages/Journal.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { View } from "react-native";
-import { Text } from "native-base";
+import textBox from "../components/textBox"
 
 const Journal = ({ navigation }: any) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Journal page</Text>
     </View>
   );
 };
