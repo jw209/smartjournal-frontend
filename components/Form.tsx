1057959bc6e55e198a032d3ee002f5075bc0401b
Jesse <jw209@uakron.edu>
2022-02-23 13:11:57 -0500

    Add emoji slider for selecting mood

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { TextArea, Text, Button, useToast, ScrollView, VStack, Container } from "native-base";
+import { TextArea, Text, Button, useToast, ScrollView, VStack, Container, Box, Stack, Slider, HStack } from "native-base";
 import { useUser } from "./UserContext";
 import supabase from "../services/supabaseClient";
 import "react-native-url-polyfill/auto";
 import moment from "moment";
+import Emoji from 'react-native-emoji'
+import Journal from "../pages/Journal";
 
 type Journal = {
   id: number
   entry: string
   created_at: string
+  mood: string
 }
+
+const emojiMap = new Map([
+  [1, "grinning"],
+  [2, "angry"],
+  [3, "open_mouth"],
+  [4, "fearful"],
+  [5, "heart_eyes"],
+  [6, "pensive"]
+])
 
 const Form = () => {
     const { user } = useUser();
     const [sendData, setSendData] = useState({});
     const [showData, setShowData] = useState<Array<Journal>>([]);
+    const [emojiName, setEmojiName] = useState('');
     const toast = useToast();
 
     useEffect(() => {
@@ -37,11 +50,11 @@ const Form = () => {
 
     // add journal to the database 
     const addJournal = async (payload: any) => {
-      const { entry, user_id, created_at } = payload;
+      const { entry, user_id, created_at, mood } = payload;
       const { data, error } = await supabase
       .from('journals')
       .insert([
-        { entry, user_id, created_at },
+        { entry, user_id, created_at, mood },
       ])
       if (error) {
         console.log(error.message);
@@ -59,15 +72,31 @@ const Form = () => {
           <TextArea placeholder="Whats on your mind?" w="xs" margin="auto" onChangeText={value => setSendData({ ...sendData,
             entry: value,
             user_id: user!.id,
-            created_at: moment().toString()
+            created_at: moment().toString(),
+            mood: emojiName
           })} maxLength={150} color="white"/>
+          <HStack space={4} alignItems="center" w="75%" maxW="300">
+            <Emoji name={emojiName} style={{fontSize: 25}} />
+            <Slider minValue={1} maxValue={6} defaultValue={1} size="lg" colorScheme="cyan" onChange={v => {
+                let myEmoji = emojiMap.get(v) as string;
+                setEmojiName(myEmoji);
+              }}>
+              <Slider.Track>
+                <Slider.FilledTrack />
+              </Slider.Track>
+              <Slider.Thumb />
+            </Slider>
+          </HStack>
           <Button width="xs" onPress={() => addJournal({...sendData})}>Add journal</Button>
           <ScrollView w="lg" maxH="77%">
             <VStack space={4} w="lg">
               {showData.map(journal => <Container key={journal.id}>
-                <Text w="xs" color="success.600">
-                  {journal.created_at}
-                </Text>
+                <HStack>
+                  <Text w="xs" color="success.600">
+                    {journal.created_at}
+                  </Text>
+                  <Emoji name={journal.mood} style={{fontSize: 25}} />
+                </HStack>
                 <Text w="xs">
                   {journal.entry}
                 </Text>
@@ -78,4 +107,5 @@ const Form = () => {
 }
 
 export default Form;
+
 
