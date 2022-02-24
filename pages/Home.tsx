4daa283dbc143bb1e40bb3b4a45ed6177c3e61d4
Jesse <jw209@uakron.edu>
2022-02-24 15:18:16 -0500

    Add basic calendar

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { View, Heading, Divider } from "native-base";
+import JournalCalendar from "../components/JournalCalendar";
 
 const Home = ({ navigation }: any) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Heading>
-        Calendar
-      </Heading>
+      <JournalCalendar />
       <Divider my="5" />
       <Heading>
         Summary
