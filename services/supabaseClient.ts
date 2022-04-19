83df9589778d75e785a429a7f61a639990081e35
Jesse <jw209@uakron.edu>
2022-04-19 14:05:11 -0400

    Remove api keys

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -2,8 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const supabase = createClient(
-  "https://bwraegmxihrflnnytmgc.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQzMjM1NDkxLCJleHAiOjE5NTg4MTE0OTF9.1V-r65flBzorokd1BAH0Sjdcxef-ZSuVo_xfEl2T9Zo",
   { localStorage: AsyncStorage as any }
 );
 
